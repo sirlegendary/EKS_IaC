899900e1cb7f4612f0ca928a773b8e19756aa866
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-23 17:42:47 +0000

    minor update

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,8 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
 module "vpc" {
   source  = "app.terraform.io/legendary-org/module-vpc/aws"
   version = "0.0.1"
   
-  aws_region                            = var.aws_region
   application_name                      = var.application_name
   application_vpc_block                 = var.application_vpc_block
   application_public_subnets_ip_lists   = var.application_public_subnets_ip_lists 
