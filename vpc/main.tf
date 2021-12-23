df0fbbe1f7159ac2f36789dd4f0cc5955f5a20c3
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-23 17:37:32 +0000

    minor update

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,6 +4,6 @@ module "vpc" {
   
   application_name                      = var.application_name
   application_vpc_block                 = var.application_vpc_block
-  application_public_subnets_ip_lists   = [var.application_public_subnets_ip_lists] 
-  application_private_subnets_ip_lists  = [var.application_private_subnets_ip_lists]
+  application_public_subnets_ip_lists   = var.application_public_subnets_ip_lists 
+  application_private_subnets_ip_lists  = var.application_private_subnets_ip_lists
 }
