5ee461bc31fce2e927d88b5c29ea0029ba2a80cc
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-24 13:21:59 +0000

    minor updates

--- cluster/config.tf
+++ cluster/config.tf
@@ -15,3 +15,18 @@ terraform{
 provider "aws" {
   region = var.aws_region
 }
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.cluster_api
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      var.clusters_name
+    ]
+  }
+}
