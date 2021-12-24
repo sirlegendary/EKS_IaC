a282606c938919f36adfcd5bfeae27c6d09f4eda
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-24 15:51:59 +0000

    minor updates

--- cluster/calculator_deployment.tf
+++ cluster/calculator_deployment.tf
@@ -1,45 +1,45 @@
-resource "kubernetes_deployment_v1" "calculator" {
-  metadata {
-    name = "calculator"
-    labels = {
-      App = "calculator"
-    }
-  }
+# resource "kubernetes_deployment_v1" "calculator" {
+#   metadata {
+#     name = "calculator"
+#     labels = {
+#       App = "calculator"
+#     }
+#   }
 
-  spec {
-    replicas = 20
-    selector {
-      match_labels = {
-        App = "calculator"
-      }
-    }
-    template {
-      metadata {
-        labels = {
-          App = "calculator"
-        }
-      }
-      spec {
-        container {
-          image = "sirlegendary/cryptocalculator"
-          name  = "calculator"
+#   spec {
+#     replicas = 20
+#     selector {
+#       match_labels = {
+#         App = "calculator"
+#       }
+#     }
+#     template {
+#       metadata {
+#         labels = {
+#           App = "calculator"
+#         }
+#       }
+#       spec {
+#         container {
+#           image = "sirlegendary/cryptocalculator"
+#           name  = "calculator"
 
-          port {
-            container_port = 80
-          }
+#           port {
+#             container_port = 80
+#           }
 
-          resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-        }
-      }
-    }
-  }
-}
+#           resources {
+#             limits = {
+#               cpu    = "0.5"
+#               memory = "512Mi"
+#             }
+#             requests = {
+#               cpu    = "250m"
+#               memory = "50Mi"
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
--- cluster/kube_service.tf
+++ cluster/kube_service.tf
@@ -1,17 +1,17 @@
-resource "kubernetes_service" "calc_service" {
-  metadata {
-    name = "calc-service"
-  }
-  spec {
-    selector = {
-      App = kubernetes_deployment_v1.calculator.metadata.0.labels.App
-    }
+# resource "kubernetes_service" "calc_service" {
+#   metadata {
+#     name = "calc-service"
+#   }
+#   spec {
+#     selector = {
+#       App = kubernetes_deployment_v1.calculator.metadata.0.labels.App
+#     }
     
-    port {
-      port        = 80
-      target_port = 80
-    }
+#     port {
+#       port        = 80
+#       target_port = 80
+#     }
 
-    type = "LoadBalancer"
-  }
-}
+#     type = "LoadBalancer"
+#   }
+# }
