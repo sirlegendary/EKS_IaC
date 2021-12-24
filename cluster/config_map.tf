53655c4e7aa2cfab18b4f8d4fa0d2c9a759f1e3f
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-24 23:50:10 +0000

    minor update

---
+++ cluster/config_map.tf
@@ -0,0 +1,13 @@
+resource "kubernetes_config_map_v1" "auth_config" {
+  metadata {
+    name = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    rolearn  = module.eks_cluster.worker_iam_role_arn
+    username = "system:node:{{EC2PrivateDNSName}}"
+    groups   = ["system:bootstrappers","system:nodes"]
+  }
+
+}
