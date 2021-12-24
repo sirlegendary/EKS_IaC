44f1e9879096eb84a0b4adc44d858ffe227dd4cb
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-24 13:26:40 +0000

    minor updates

--- cluster/config.tf
+++ cluster/config.tf
@@ -18,7 +18,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host                   = module.eks_cluster.cluster_api
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   exec {
     api_version = "client.authentication.k8s.io/v1alpha1"
     command     = "aws"
