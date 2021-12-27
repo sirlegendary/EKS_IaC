4c4fdfea900baf690cd448f3c97a210aa08a421e
Wale Salami <walesalami@Wales-MBP-2.lan>
2021-12-27 17:50:11 +0000

    minor updates

--- cluster/config_map.tf
+++ cluster/config_map.tf
@@ -6,11 +6,11 @@ resource "kubernetes_config_map_v1" "auth_config" {
   data = {
     api_host = module.eks_cluster.cluster_api
         mapRoles =<<YAML
-            - groups:
+          - rolearn: ${module.eks_cluster.worker_iam_role_arn}
+            username: system:node:{{EC2PrivateDNSName}}
+            groups:
               - system:bootstrappers
               - system:nodes
-            rolearn: ${module.eks_cluster.worker_iam_role_arn}
-            username: system:node:{{EC2PrivateDNSName}}
         YAML
   }
   depends_on = [ module.eks_cluster.cluster_api ]
