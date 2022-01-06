494ab970c8e5503a32da9c75be25c63420f7aba1
Wale Salami <walesalami@Wales-MBP-2.lan>
2022-01-06 17:31:09 +0000

    minor update to s3

--- cluster/s3.tf
+++ cluster/s3.tf
@@ -1,7 +1,73 @@
 resource "aws_s3_bucket" "eks_s3" {
   bucket = "eks-django-application-bucket"
   acl    = "public-read-write"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST", "DELETE"]
+    allowed_origins = ["*"]
+    expose_headers  = []
+    max_age_seconds = 3000
+  }
+
 }
+# CORS
+# [
+#     {
+#         "AllowedHeaders": [
+#             "*"
+#         ],
+#         "AllowedMethods": [
+#             "PUT",
+#             "POST",
+#             "DELETE"
+#         ],
+#         "AllowedOrigins": [
+#             "*"
+#         ],
+#         "ExposeHeaders": []
+#     },
+#     {
+#         "AllowedHeaders": [
+#             "*"
+#         ],
+#         "AllowedMethods": [
+#             "PUT",
+#             "POST",
+#             "DELETE"
+#         ],
+#         "AllowedOrigins": [
+#             "*"
+#         ],
+#         "ExposeHeaders": []
+#     },
+#     {
+#         "AllowedHeaders": [],
+#         "AllowedMethods": [
+#             "GET"
+#         ],
+#         "AllowedOrigins": [
+#             "*"
+#         ],
+#         "ExposeHeaders": []
+#     }
+# ]
+
+# {
+#   "Id": "Policy1641144620446",
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "Stmt1641143669439",
+#       "Action": [
+#         "s3:GetObject"
+#       ],
+#       "Effect": "Allow",
+#       "Resource": "arn:aws:s3:::eks-django-application-bucket",
+#       "Principal": "*"
+#     }
+#   ]
+# }
 
 # resource "aws_iam_role" "eks_role" {
 #   name = "eks_role"
